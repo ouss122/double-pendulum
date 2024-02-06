f9d9ef4bb5c05ceeaa7706bbbf532ca0efe6c2ff
ouss122 <s.oussama.salmi@gmail.com>
2024-02-06 23:59:09 +0100

    improved the UI

--- public/ch7.r
+++ public/ch7.r
@@ -2,12 +2,14 @@ m1 <- 1
 m2 <- 1
 L1 <- 1.5
 L2 <- 1
+a1<-3.14
+a2<-0.05
 g <- 9.8
-x <- c(0,0,pi,0.05)
+x <- c(0,0,a1,a2)
+# x <- 1
 t <- 0
 
 runge_kutta<- function (f,h,n=1){
-
 
     while (n>0){
         k1<-h*f(x,t)
@@ -16,12 +18,39 @@ runge_kutta<- function (f,h,n=1){
         k4<-h*f(x+k3,t+h)
 
         x <<- x + (1/6)*(k1+2*k2+2*k3+k4)
-        t<<- t + h
+        t <<- t + h
         n <- n-1
     }
     
 }
 
+euler <- function (f,h,n=1){
+    while (n>0){
+        x <<- x + h*f(x,t)
+        t <<- t + h
+        n <- n-1
+    } 
+}
+
+euler_modifie<- function(f,h,n=1){
+    while (n>0){
+        k<-f(x,t)
+        k2<-f(x+h*k,t+h)
+        x <<- x +(h/2)*(k+k2)
+        t <<- t + h
+        n <- n-1
+    }
+}
+
+euler_ameliore<-function(f,h,n=1){
+    while(n>0){
+        k <- f(x,t)
+        k2 <- f(x+(h/2)*k,t+(h/2))
+        x <<- x + h*k2
+        t <<- t+h
+        n <- n-1
+    }
+}
 
 
 double_pendulum_o1 <- function (o1,o2,t1,t2) {
@@ -49,9 +78,41 @@ f<- function (x,t){
 }
 
 
-get_cords <- function (delta){
+get_cords_rk4 <- function (delta){
     runge_kutta(f,delta)
     return(c(x[3],x[4]))
 }
 
+get_cords_euler <- function (delta){
+    euler(f,delta);
+    return(c(x[3],x[4]))
+}
 
+get_cords_eulerM <- function (delta){
+    euler_modifie(f,delta);
+    return(c(x[3],x[4]))
+}
+
+get_cords_eulerA <- function (delta){
+    euler_ameliore(f,delta);
+    return(c(x[3],x[4]))
+}
+
+# g <- function(x,t){
+#     return(-x+t+1)
+# }
+# g <- function(x,t){
+#     return(x+t)
+# }
+
+
+
+# euler(g,0.1,11)
+# euler_ameliore(g,0.1,11)
+# euler_modifie(g,0.1,11)
+
+
+#   dtet <- t1-t2
+#    e<- expression((m2*L1*(o1^2)*sin(2*dtet)+2*m2*L2*(o2^2)*sin(dtet)+2*g*m2*cos(t2)*sin(dtet)+2*g*m1*sin(t1))/(-2*L1*(m1+m2*(sin(dtet)^2))))
+
+deriv(f,"t")
--- src/main.ts
+++ src/main.ts
@@ -2,31 +2,155 @@ import './style.css'
 import p5 from "p5"
 import {RDouble, RFunction, WebR} from "webr"
 
+
+let start=false;
+
+let selected=3;
 let element=document.getElementById("app") as HTMLDivElement
 
-
+let bMass=document.getElementById("bMass") as HTMLInputElement;
+let rMass=document.getElementById("rMass") as HTMLInputElement;
+let l1Length=document.getElementById("l1Length") as HTMLInputElement;
+let l2Length=document.getElementById("l2Length") as HTMLInputElement;
+let Ang1=document.getElementById("Ang1") as HTMLInputElement;
+let Ang2=document.getElementById("Ang2") as HTMLInputElement;
+let explicite=document.getElementById("explicite") as HTMLHeadingElement;
+let ameliore=document.getElementById("ameliore") as HTMLHeadingElement;
+let modifie=document.getElementById("modifie") as HTMLHeadingElement;
+let rk4=document.getElementById("rk4") as HTMLHeadingElement;
+let st=document.getElementById("start") as HTMLHeadingElement;
+let a1=Ang1.valueAsNumber;
+let a2=Ang2.valueAsNumber;
+const red ="#b0453b"
+const blue ="#5f95ca"
+const black = "#4c4c4c"
+let methods=[explicite,ameliore,modifie,rk4]
 const webR = new WebR();
-
-
 await webR.init();
-
-
 let rFunction=await fetch("ch7.r");
-
 webR.evalR(await rFunction.text())
 
-let  get_cords=await webR.evalR('get_cords') as RFunction;
+
+async function calculate(delta:number){
+  let k;
+  switch (selected) {
+    case 0:
+        k=await webR.evalR(`get_cords_euler(${delta})`) as RDouble;
+      break;
+    case 1:
+        k=await webR.evalR(`get_cords_eulerM(${delta})`) as RDouble;
+      break;
+    case 2:
+      k=await webR.evalR(`get_cords_eulerA(${delta})`) as RDouble;
+      break;
+    case 3:
+      k=await webR.evalR(`get_cords_rk4(${delta})`) as RDouble;
+      break;
+    default:
+      break;
+  }
+  return k;
+}
+
+explicite.addEventListener("click",async ()=>{
+  if (selected !==0&&!start){
+    methods[selected].classList.remove("selected")
+    selected=0;
+    explicite.classList.add("selected")
+    await webR.evalR("x<-c(0,0,x[3],x[4])")
+  }
+})
+
+ameliore.addEventListener("click",async ()=>{
+  if (selected !==1&&!start){
+    methods[selected].classList.remove("selected")
+    selected=1;
+    ameliore.classList.add("selected")
+    await webR.evalR("x<-c(0,0,x[3],x[4])")
+  }
+})
+
+modifie.addEventListener("click",async ()=>{
+  if (selected !==2&&!start){
+    methods[selected].classList.remove("selected")
+    selected=2;
+    modifie.classList.add("selected")
+    await webR.evalR("x<-c(0,0,x[3],x[4])")
+  }
+})
+
+rk4.addEventListener("click",async ()=>{
+  if (selected !==3 &&!start){
+    methods[selected].classList.remove("selected")
+    selected=3;
+    rk4.classList.add("selected")
+    await webR.evalR("x<-c(0,0,x[3],x[4])")
+  }
+})
+
+//////////////////////////////////////////////////////////////////
+
+
+bMass.addEventListener("change",()=>{
+  webR.evalR(`m1<-${bMass.valueAsNumber};x<-c(0,0,x[3],x[4])`)
+})
+
+rMass.addEventListener("change",()=>{
+  webR.evalR(`m2<-${rMass.valueAsNumber};x<-c(0,0,x[3],x[4])`)
+})
+
+l1Length.addEventListener("change",()=>{
+  webR.evalR(`l1<-${l1Length.valueAsNumber};x<-c(0,0,x[3],x[4])`)
+})
+
+l2Length.addEventListener("change",()=>{
+  webR.evalR(`l2<-${l2Length.valueAsNumber};x<-c(0,0,x[3],x[4])`)
+})
+
+Ang1.addEventListener("change",()=>{
+  webR.evalR(`a1<-${Ang1.valueAsNumber}`)
+  a1=Ang1.valueAsNumber
+})
+
+Ang2.addEventListener("change",()=>{
+  webR.evalR(`a2<-${Ang2.valueAsNumber}`)
+  a2=Ang2.valueAsNumber
+})
+
+st.addEventListener("click",()=>{
+  if (start){
+    start=false;
+    st.innerText="Start";
+    bMass.disabled=false;
+    rMass.disabled=false;
+    l1Length.disabled=false;
+    l2Length.disabled=false;
+    Ang1.disabled=false;
+    Ang2.disabled=false;
+  }else{
+    webR.evalR(`a1<-${a1}`)
+    webR.evalR(`a2<-${a2}`)
+    webR.evalR("x<-c(x[1],x[2],a1,a2)")
+    start=true;
+    st.innerText="Stop"
+    bMass.disabled=true;
+    rMass.disabled=true;
+    l1Length.disabled=true;
+    l2Length.disabled=true;
+    Ang1.disabled=true;
+    Ang2.disabled=true;
+  }
+})
+
+
 
 
 console.log('hello');
 
-// console.log(await get_cords());
-
-
 new p5((p:p5) => {
   let px0=p.width/2;
-  let L1=1.5;
-  let L2=1;
+  // let L1=1.5;
+  // let L2=1;
   let py0=200;
   let pt1=p.PI;
   let pt2=0.05;
@@ -34,51 +158,84 @@ new p5((p:p5) => {
 
     p.setup = () => {
       
+    let L1=l1Length.valueAsNumber;
+    let L2=l2Length.valueAsNumber;
     p.createCanvas(element.clientWidth,element.clientHeight).parent(element)
     px0=p.width/2;
-    p.fill(200)
-    p.stroke(200)
+    // p.fill(200)
+    // p.stroke(200)
     
     let x1=(L1*p.sin(pt1))*sc +px0;
     let y1=L1*p.cos(pt1)*sc +py0;
     let x2=x1+ L2*p.sin(pt2)*sc;
     let y2=y1 +L2*p.cos(pt2)*sc;    
 
-    p.circle(px0,py0,20)
+    p.stroke(black)
     p.line(px0,py0,x1,y1)
-    p.circle(x1,y1,40)
     p.line(x1,y1,x2,y2)
+    p.fill(black)
+    p.circle(px0,py0,20)
+    p.fill(blue)
+    p.circle(x1,y1,40)
+    p.fill(red)
     p.circle(x2,y2,40) 
   }
 //   x1 <- L1*sin(x[3])
 // y1 <- -L1*cos(x[3])
 // x2 <- x1 + L2*sin(x[4])
 // y2 <- y1 - L2*cos(x[4])
-  p.draw = async () => {
-    p.fill(200)
-    p.stroke(200)
-    // let x=await get_cords()
-    // console.log(p.deltaTime);
-    
-    let cords=await webR.evalR(`get_cords(${p.deltaTime*1.2/1000})`) as RDouble;
-    // let cords=await get_cords(p.deltaTime/1000) as RDouble
-    
-    // console.log(co);
-    
-    let [pt1,pt2]=await cords.toArray() as number[]
-    
-    
-    p.background(10,10,10)
-    
-    let x1=(L1*p.sin(pt1))*sc +px0;
-    let y1=L1*p.cos(pt1)*sc +py0;
-    let x2=x1+ L2*p.sin(pt2)*sc;
-    let y2=y1 +L2*p.cos(pt2)*sc;    
+ p.draw = async () => {
+  let L1=l1Length.valueAsNumber;
+  let L2=l2Length.valueAsNumber;
 
-    p.circle(px0,py0,20)
-    p.line(px0,py0,x1,y1)
-    p.circle(x1,y1,40)
-    p.line(x1,y1,x2,y2)
-    p.circle(x2,y2,40) 
+    if (start){
+      // let cords=await webR.evalR(`get_cords(${p.deltaTime*1.2/1000})`) as RDouble;
+      let cords=await calculate(p.deltaTime*1.2/1000)
+      let [pt1,pt2]=await (cords as RDouble).toArray() as number[];
+
+      a1=pt1;
+      a2=pt2;
+      if (Math.abs(Ang1.valueAsNumber-pt1)>0.2){        
+        Ang1.valueAsNumber=pt1;
+        Ang2.valueAsNumber=pt2;
+      }
+      p.background("#c8c8c8")
+      
+      let x1=(L1*p.sin(pt1))*sc +px0;
+      let y1=L1*p.cos(pt1)*sc +py0;
+      let x2=x1+ L2*p.sin(pt2)*sc;
+      let y2=y1 +L2*p.cos(pt2)*sc;    
+  
+  
+      p.stroke(black)
+      p.line(px0,py0,x1,y1)
+      p.line(x1,y1,x2,y2)
+      p.fill(black)
+      p.circle(px0,py0,20)
+      p.fill(blue)
+      p.circle(x1,y1,40)
+      p.fill(red)
+      p.circle(x2,y2,40) 
+    }else{
+
+      p.background("#c8c8c8")
+
+      let x1=(L1*p.sin(a1))*sc +px0;
+      let y1=L1*p.cos(a1)*sc +py0;
+      let x2=x1+ L2*p.sin(a2)*sc;
+      let y2=y1 +L2*p.cos(a2)*sc;    
+  
+  
+      p.stroke(black)
+      p.line(px0,py0,x1,y1)
+      p.line(x1,y1,x2,y2)
+      p.fill(black)
+      p.circle(px0,py0,20)
+      p.fill(blue)
+      p.circle(x1,y1,40)
+      p.fill(red)
+      p.circle(x2,y2,40) 
+    }
+
   }
 })
